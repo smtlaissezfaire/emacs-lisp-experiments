9e08ab674e814169ac5f77cc2bce8ab86ec49126
Scott Taylor <smt@scott-taylors-macbook.local>
2008-05-02 04:39:21 -0400

    * Almost done with ido-find-in-project

--- ido-find-in-project.el
+++ ido-find-in-project.el
@@ -4,37 +4,43 @@
 ;; 2. Use an interactive fuzzy-match against a string
 ;; 3. Make it interactive, so that files can be cycled through
 
+(defvar project-root "~/src/svn/flavorpill/flavorpill_com/trunk")
+
 (defun fip-fuzzy-match (string list)
   (fip-match-with-regexp (explode-to-regexp string) list))
-
-(fip-match-with-regexp "foobar" (directory-files "."))
 
 (defun empty-p (list)
   (eq list nil))
 
 (defun fip-match-with-regexp (regexp list)
   "Return the elements of list which match the regular expression regexp"
-  (cond ((empty-p list)
-         (quote ()))
-        ((eql-match-p regexp (car list))
-         (cons (car (list))
-               (fip-match-with-regexp (regexp (cdr list)))))
-        (fip-match-with-regexp (regexp (cdr list)))))
+  (defun eql-match-p (string)
+    "Equivalent to ruby's =~"
+    (with-temp-buffer
+      (insert string)
+      (goto-char (point-min))
+      (search-forward-regexp regexp (point-max) t)))
+  (remove-if-not 'eql-match-p list))
 
-(defun eql-match-p (regexp string)
-  "Equivalent to ruby's =~"
-  (with-temp-buffer
-    (insert string)
-    (goto-char (point-min))
-    (search-forward-regexp regexp (point-max) t)))
 
 (defun explode-to-regexp (string)
   "Explode a string to a regex, where each char has a .* in front and back of it"
   (regexp-quote (apply 'concat (explode-to-regexp-list string))))
 
 (defun explode-to-regexp-list (string)
-  (cons ".*" 
+  (cons "\.\*" 
         (mapcar 'char-exploded-to-regexp string)))
 
-(defun fip-char-exploded-to-regexp (char)
+(defun char-exploded-to-regexp (char)
    (concat (string char) ".*"))
+
+(defun recursive-find-files (directory-root)
+  (with-temp-buffer
+    (cd directory-root)
+    (shell-command "find app config db lib test spec | grep -v .svn" (current-buffer))
+    (split-string (buffer-string))))
+
+(find-all-files project-root)
+
+(fip-fuzzy-match "app" (find-all-files project-root))
+
