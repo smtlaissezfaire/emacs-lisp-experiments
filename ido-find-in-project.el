9b1d3a02c45ed47bd1601dcd6aeeb57ca44c5910
Scott Taylor <smt@scott-taylors-macbook.local>
2008-05-06 02:12:23 -0400

    * Better code - now actually finds all the appropriate files

--- ido-find-in-project.el
+++ ido-find-in-project.el
@@ -12,16 +12,16 @@
 (defun empty-p (list)
   (eq list nil))
 
+(defun eql-match-p (string regexp)
+  "Tests for regexp equality"
+  (with-temp-buffer
+    (insert string)
+    (goto-char (point-min))
+    (search-forward-regexp regexp (point-max) t)))
+
 (defun fip-match-with-regexp (regexp list)
   "Return the elements of list which match the regular expression regexp"
-  (defun eql-match-p (string)
-    "Equivalent to ruby's =~"
-    (with-temp-buffer
-      (insert string)
-      (goto-char (point-min))
-      (search-forward-regexp regexp (point-max) t)))
-  (remove-if-not 'eql-match-p list))
-
+  (remove-if-not (lambda (x) (eql-match-p list regexp)) list))
 
 (defun explode-to-regexp (string)
   "Explode a string to a regex, where each char has a .* in front and back of it"
@@ -37,10 +37,38 @@
 (defun recursive-find-files (directory-root)
   (with-temp-buffer
     (cd directory-root)
-    (shell-command "find app config db lib test spec | grep -v .svn" (current-buffer))
+    (shell-command "find . | grep -v .svn | grep -v vendor | grep .rb" (current-buffer))
     (split-string (buffer-string))))
+
+(defun basename (file)
+  (cond ((string-with-slash-p file)
+         (basename (strip-to-slash file)))
+        (file)))
+
+(defun string-with-slash-p (string)
+  (not (eq (search "/" string) nil)))
+
+(defun strip-to-slash (string)
+  (let ((string-length (length string))
+        (string-search (search "/" string)))
+    (cond ((not (eq string-search nil))
+           (subseq string
+                   (string-offset string-search)
+                   string-length))
+          (string))))
+
+(defun string-offset (counter)
+  (+ 1 counter))
+                
+(mapcar (lambda (file) (basename file)) (recursive-find-files project-root))
+
+(defun find-all-files (project-root)
+  "Find all the files in a project, and create a list of dotted pairs,
+with the complete path name as the car, and the abbreviated path name as the cdr"
+  (mapcar
+   (lambda (file) (list file (basename file)))
+   (recursive-find-files project-root)))
 
 (find-all-files project-root)
 
-(fip-fuzzy-match "app" (find-all-files project-root))
 
