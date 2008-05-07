8c639aed0cbeccdf45647ea550fd9e3d024df37b
Scott Taylor <smt@scott-taylors-macbook.local>
2008-05-07 04:05:06 -0400

    * Moving some of the old code out into a fuzzy-matching file

--- ido-find-in-project.el
+++ ido-find-in-project.el
@@ -41,35 +41,6 @@ strings to choose from."
     (assoc basename (find-files (rails-root))))
   (find-file (concat (rails-root) (lookup-file))))
 
-(defun find-in-project (prefix)
-  (cond ((rails-root)
-         (fip-fuzzy-match prefix
-                          (find-files (rails-root))))))
-
-(defun fip-fuzzy-match (string list)
-  (fip-match-with-regexp (explode-to-regexp string)
-                         (mapcar (lambda (lst) (car lst))
-                                 list)))
-
-(defun fip-match-with-regexp (regexp lst)
-  "Return the elements of list lst which match the regular expression regexp"
-  (remove-if-not
-   (lambda (str) 
-     (eql-match-p str regexp)) 
-   lst))
-
-(defun explode-to-regexp (string)
-  "Explode a string to a regular expression, where each char has a .* in front and back of it"
-  (apply 'concat (explode-to-regexp-list string)))
-
-(defun explode-to-regexp-list (string)
-  "Explode a string to a list of chars, where each char has a .* in front and back of it"
-  (cons ".*" 
-        (mapcar 'char-exploded-to-regexp string)))
-
-(defun char-exploded-to-regexp (char)
-   (concat (string char) ".*"))
-
 (defun find-all-files (directory-root &optional shell-commandf)
   (unless shell-commandf 
     (setf shell-commandf "find ."))
