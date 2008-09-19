e5fce6a81d23e2d777988d3c593dcb754b0a2c9e
Scott Taylor <scott@railsnewbie.com>
2008-09-19 02:01:48 -0400

    * move gsub into utils.el

--- utils.el
+++ utils.el
@@ -44,3 +44,14 @@
   (end-of-buffer)
   (indent-region (point-min)
                  (point-max)))
+
+; Ripped off from steve yegge's blog...thanks to whoever wrote it
+(defun gsub (search-string replace string &optional regexp-flag)
+  "Like Ruby gsub."
+  (with-temp-buffer
+    (insert string)
+    (goto-char (point-min))
+    (let ((search-function (if regexp-flag 're-search-forward 'search-forward)))
+      (while (funcall search-function search-string nil t)
+        (replace-match replace))
+      (buffer-string))))
