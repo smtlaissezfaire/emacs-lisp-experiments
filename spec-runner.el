773836ae9799c94e67fdf054479f6b77cd7d4449
Scott Taylor <scott@railsnewbie.com>
2008-09-19 02:05:13 -0400

    * Remove life-hack, spec-runner

--- spec-runner.el
+++
@@ -1,7 +0,0 @@
-
-(defun run-specs ()
-  (interactive)
-  (shell-command (concat "~/src/svn/flavorpill/flavorpill_com/trunk/script/spec" " "
-                         "--drb" " "
-                         "--color" " "
-                         buffer-file-name)))
